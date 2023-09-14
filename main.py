0b702a507c9f63e3a7e3461344fcbdeee7e47d46
itota37 <40158522+itota37@users.noreply.github.com>
2023-09-14 03:18:13 +0000

    コメントを書き直しました。

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
+# Pygameで横スクロールアクションを作ろう!!
+
 import pygame
 import sys
 
-# pygameの初期化
-pygame.init()
 
-# ゲームウィンドウのサイズ
-screen_width = 800
-screen_height = 600
+# main関数
+def main():
 
-# ゲームウィンドウの作成
-screen = pygame.display.set_mode((screen_width, screen_height))
+  # -------------------------
+  #
+  # 初期化する
+  #
 
-# ゲームループ
-running = True
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+  # Pygameの初期化する
+  pygame.init()
 
-  # ゲームのロジックをここに追加
+  # スクリーンのサイズを決める
+  screen_width = 800  # スクリーンの幅
+  screen_height = 600  # スクリーンの高さ
 
-  # ゲーム画面を更新
-  pygame.display.flip()
+  # ゲームウィンドウの作成する
+  # 作成するときにスクリーンの幅と高さを設定する
+  screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Pygameの終了
-pygame.quit()
-sys.exit()
+  # -------------------------
+  #
+  # 更新する
+  #
+
+  # ゲームループ
+  running = True
+  while running:  # runningがTrueの間、回し続ける
+
+    # イベントを処理する
+    for event in pygame.event.get():
+
+      # 終了イベントならば
+      if event.type == pygame.QUIT:
+        # runningをFalseにする
+        running = False
+
+    # for event はここまで
+
+    # ゲームのロジックをここに追加
+
+    # ゲーム画面を更新する
+    pygame.display.flip()
+
+  # while running はここまで
+
+  # -------------------------
+  #
+  # 終了する
+  #
+
+  # Pygameを終了する
+  pygame.quit()
+
+  # アプリケーションを終了する
+  sys.exit()
+
+
+# mainはここまで
+
+if __name__ == '__main__':
+
+  # mainを呼び出す
+  main()
